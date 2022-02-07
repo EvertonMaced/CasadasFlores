4bf495b191aae8a45c78eeeda53c2437765e610d
EVERTON DULTRA MACEDO <everton.dmacedo@senacsp.edu.br>
2022-02-07 11:21:51 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/casadasflores/MeunuPrincipal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.casadasflores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeunuPrincipal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meunu_principal_layout);
+    }
+}
